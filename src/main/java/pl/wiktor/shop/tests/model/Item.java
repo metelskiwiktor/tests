51290fdba6fc105bc8b3ac6fbcaebd7969f6a315
metelskiw <wiktor.metelski@lideo.tech>
2020-01-07 17:09:14 +0100

    added basic step

--- src/main/java/pl/wiktor/shop/tests/model/Item.java
+++ src/main/java/pl/wiktor/shop/tests/model/Item.java
@@ -13,5 +13,35 @@ public class Item {
     private Stock stock;
     private Set<Author> authors;
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBasicPrice(BigDecimal basicPrice) {
+        this.basicPrice = basicPrice;
+    }
+
+    public void setDiscount(Discount discount) {
+        this.discount = discount;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
+
+    public void setTags(Set<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public void setStock(Stock stock) {
+        this.stock = stock;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
 }
--- src/test/resources/feature/item/item.feature
+++ src/test/resources/feature/item/item.feature
@@ -2,3 +2,4 @@ Feature: Item microservice can add items, authors, tags, categories
 
   Scenario: Item is added
     Given I have item
+    When I send request to add item
